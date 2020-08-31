65c90270d0ee819116b6676831fb53eba55e49ce
Asma Srour <asma.srour96@gmail.com>
2020-08-31 14:13:53 +0300

    Updates

---
+++ precache-manifest.3249dd1486cb79a034cc2e3231351fdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "931fb1082b78233c1e0eba26431ffcba",
+    "url": "/Page/index.html"
+  },
+  {
+    "revision": "54689881c024171f79bb",
+    "url": "/Page/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "cb1368e5e411b44c73b2",
+    "url": "/Page/static/css/main.3af4b3ba.chunk.css"
+  },
+  {
+    "revision": "54689881c024171f79bb",
+    "url": "/Page/static/js/2.62fec23c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Page/static/js/2.62fec23c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb1368e5e411b44c73b2",
+    "url": "/Page/static/js/main.5d55eec7.chunk.js"
+  },
+  {
+    "revision": "bdc0a3950baa2f03910e",
+    "url": "/Page/static/js/runtime-main.b643f76a.js"
+  }
+]);
